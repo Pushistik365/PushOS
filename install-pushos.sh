f8a9208292ccfb4a6fa8995c74b4cc6416e441a7
Pushistik365 <90297576+Pushistik365@users.noreply.github.com>
2022-08-17 22:45:14 +0300

    Add files via upload

---
+++ install-pushos.sh
@@ -0,0 +1,39 @@
+# update repos
+apt update
+apt upgrade
+pkg update
+pkg upgrade
+
+# install additional repositories
+pkg install root-repo
+pkg install x11-repo
+
+# install desktop environment and vnc server
+pkg install tigervnc --no-install-recommends
+pkg install xfce4 --no-install-recommends
+pkg install xfce4-terminal --no-install-recommends
+
+# unpack rootfs archive
+mv pushos-rootfs.tar ~/
+cd ~/
+tar -xvf pushos-rootfs.tar
+
+# create splash screen
+cd $PREFIX/etc/
+rm -rf motd*
+pkg install figlet
+echo "figlet -f small PushOS" >> bash.bashrc
+echo "echo Start PushOS: pushos-start" >> bash.bashrc
+echo "echo Stop PushOS: pushos-stop" >> bash.bashrc
+echo "PushOS geometry: 1380x720" >> bash.bashrc
+
+# create start and stop files
+cd $PREFIX/bin/
+echo "vncserver -geometry 1380x720" > pushos-start
+echo "vncserver -kill :1" > pushos-stop
+
+# remove temporary files
+cd ~/
+rm -rf pushos-rootfs.tar
+rm -rf PushOS/
+
